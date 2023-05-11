9886da7019946de6e5871e5e979a545fd04748d3
SofiaPV <sofiavpobar@gmail.com>
2023-05-11 20:55:33 +0300

    working side bar

--- MenuController.h
+++ MenuController.h
@@ -1,4 +1,20 @@
 #ifndef MENUCONTROLLER_H
 #define MENUCONTROLLER_H
 
+#include <QAction>
+#include <QMenuBar>
+#include <QToolBar>
+
+class MenuController:public QObject {
+    Q_OBJECT
+private:
+
+public:
+
+signals:
+
+
+};
+
 #endif // MENUCONTROLLER_H
+//QObject
